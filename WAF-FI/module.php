3d67235627520842891d6d5b91cb4d053b62d692
Nisbo <webmaster@nissanboard.de>
2019-05-19 09:32:47 +0200

    Update module.php

--- WAF-FI/module.php
+++ WAF-FI/module.php
@@ -1,15 +1,68 @@
 <?
-    // Klassendefinition
-    class IPS_Waf_FernsehInterface extends IPSModule {
-        /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-        *
-        * ABC_MeineErsteEigeneFunktion($id);
-        *
-        */
-        public function generateHTMLcontent() {
-            echo $this->InstanceID;
-        }
-    }
+// Klassendefinition
+class IPS_Waf_FernsehInterface extends IPSModule {
+
+
+
+	public function Create() {
+		//Never delete this line!
+		parent::Create();
+
+		$this->RegisterPropertyString("backButton",						"Back");
+		$this->RegisterPropertyInteger("channelDeviceObjectID",			0);
+		$this->RegisterPropertyString("channelDown",					"ChannelDown");
+		$this->RegisterPropertyString("channelListFileName",			"Kanalliste.csv");
+		$this->RegisterPropertyString("channelUp",						"ChannelUp");
+		$this->RegisterPropertyString("colorButtonBlue",				"Blue");
+		$this->RegisterPropertyString("colorButtonGreen",				"Green");
+		$this->RegisterPropertyString("colorButtonRed",					"Red");
+		$this->RegisterPropertyString("colorButtonYellow",				"Yellow");
+		$this->RegisterPropertyString("directionDown",					"DirectionDown");
+		$this->RegisterPropertyString("directionLeft",					"DirectionLeft");
+		$this->RegisterPropertyString("directionRight",					"DirectionRight");
+		$this->RegisterPropertyString("directionUp",					"DirectionUp");
+		$this->RegisterPropertyInteger("harmonyHubObjectID",			0);
+		$this->RegisterPropertyInteger("harmonyHubStartActivityOn",		0);
+		$this->RegisterPropertyInteger("harmonyHubStartActivityOff",	0);
+		$this->RegisterPropertyString("okButton",						"Select");
+		$this->RegisterPropertyString("pathToCSV",						"../webfront/user/waffi");
+		$this->RegisterPropertyString("pathToImages",					"user/waffi/images");
+		$this->RegisterPropertyBoolean("sendENTERkey",					true);
+		$this->RegisterPropertyString("sendENTERkeyCode",				"Select");
+		$this->RegisterPropertyBoolean("sendSingleDigits",				true);
+		$this->RegisterPropertyInteger("volumeDeviceObjectID",			0);
+		$this->RegisterPropertyString("volumeDown",						"VolumeDown");
+		$this->RegisterPropertyString("volumeMute",						"Mute");
+		$this->RegisterPropertyString("volumeUp",						"VolumeUp");
+
+		$this->RegisterVariableString("channelListHTML", "Bitte Config ausführen", "~HTMLBox", 1);
+		//$this->RegisterVariableFloat("value2",		"Value No 2", "", 2);
+		//$this->RegisterVariableFloat("value3",		"Value No 3", "", 3);
+	}
+
+	public function ApplyChanges() {
+		//Never delete this line!
+		parent::ApplyChanges();
+	}
+
+
+
+	/**
+	* Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+	* Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+	*
+	* ABC_MeineErsteEigeneFunktion($id);
+	*
+	*/
+	public function generateHTMLcontent() {
+		echo $this->InstanceID;
+	}
+
+
+
+
+
+
+
+}
 ?>
