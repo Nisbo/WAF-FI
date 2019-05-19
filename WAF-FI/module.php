029a5cc20f8c511bd24320d2971defdb1cafa501
Nisbo <webmaster@nissanboard.de>
2019-05-19 12:10:10 +0200

    Update module.php

--- WAF-FI/module.php
+++ WAF-FI/module.php
@@ -11,6 +11,8 @@
 // https://www.iconfinder.com/iconsets/e-commerce-icon-set
 // https://www.iconfinder.com/iconsets/developer-set-3
 
+require_once __DIR__ . '/../libs/WafFiHelper.php';
+
 // Klassendefinition
 class IPS_Waf_FernsehInterface extends IPSModule {
 
@@ -375,57 +377,7 @@ class IPS_Waf_FernsehInterface extends IPSModule {
 
 		//echo $channelListHTML;
 
-		// the Class CsvImporter is based on a code found on PHP.net 
-		class CsvImporter {
-			private $fp;
-			private $parse_header;
-			private $header;
-			private $delimiter;
-			private $length;
 
-			function __construct($file_name, $parse_header=false, $delimiter=";", $length=8000) {
-				$this->fp = fopen($file_name, "r");
-				$this->parse_header = $parse_header;
-				$this->delimiter    = $delimiter;
-				$this->length       = $length;
-
-				if ($this->parse_header) {
-				   $this->header = fgetcsv($this->fp, $this->length, $this->delimiter);
-				}
-
-			}
-
-			function __destruct() {
-				if ($this->fp) {
-					fclose($this->fp);
-				}
-			}
-
-			function get($max_lines=0) {
-				$data = array();
-
-				if ($max_lines > 0) {
-					$line_count = 0;
-				} else {
-					$line_count = -1; // so loop limit is ignored
-				}
-
-				while ($line_count < $max_lines && ($row = fgetcsv($this->fp, $this->length, $this->delimiter)) !== FALSE) {
-					if ($this->parse_header) {
-						foreach ($this->header as $i => $heading_i) {
-							$row_new[$heading_i] = $row[$i];
-						}
-						$data[] = $row_new;
-					} else {
-						$data[] = $row;
-					}
-
-					if ($max_lines > 0)
-						$line_count++;
-				}
-				return $data;
-			}
-		}
 
 	}
 
