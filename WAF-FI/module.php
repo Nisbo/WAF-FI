5ff0c5661e51e237c951944813c9b80995d01732
Nisbo <webmaster@nissanboard.de>
2019-05-27 17:14:59 +0200

    Update module.php

--- WAF-FI/module.php
+++ WAF-FI/module.php
@@ -49,7 +49,7 @@ class IPS_Waf_FernsehInterface extends IPSModule {
 		$this->RegisterPropertyBoolean("useBouquet",					false);
 		
 
-		$this->RegisterPropertyString("designButtonBackGroundColor", "D3DAD9");
+		$this->RegisterPropertyInteger("designButtonBackGroundColor", "D3DAD9");
 
 
 		$this->RegisterVariableString("channelListHTML",				"Diese Variabel ins FrontEnd einbinden, zum Ändern bitte Config ausführen", "~HTMLBox", 1);
@@ -183,7 +183,7 @@ class IPS_Waf_FernsehInterface extends IPSModule {
 
 		$useBouquet				= $this->ReadPropertyBoolean("useBouquet");
 
-$designButtonBackGroundColor = $this->ReadPropertyString("designButtonBackGroundColor");
+$designButtonBackGroundColor = $this->ReadPropertyInteger("designButtonBackGroundColor");
 		
 		
 		if(substr($pathToImages, -1, 1) != "/") $pathToImages          .= "/"; // to make sure there is a slash at the end
